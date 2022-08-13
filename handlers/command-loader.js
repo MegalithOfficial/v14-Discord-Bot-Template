20f9dcc28eae03adc297869b02c0307554d5436b
TheSid <74655120+TheSidOffical@users.noreply.github.com>
2022-08-13 10:30:16 +0300

    Update command-loader.js

--- handlers/command-loader.js
+++ handlers/command-loader.js
@@ -29,13 +29,13 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   try {
-    if(HandlerMode === "Guild") {
+    if(handlerMode.toLocaleUpperCase == "GUILD" || handlerMode.toLowerCase == "guild" || handlerMode === "Guild") {
 
     await rest.put(
       Routes.applicationGuildCommands(appId, guildId),
       { body: commands }
     )
-    } else if(HandlerMode === "Global") {
+    } else if(handlerMode.toLocaleUpperCase == "GLOBAL" || handlerMode.toLowerCase == "global" || handlerMode === "Global") {
 
       await rest.put(
         Routes.applicationCommands(appId),
