e1af7ddc596aaf92d9af3fad07e14038764a933e
mustafa383 <57295889+mustafa383@users.noreply.github.com>
2023-02-19 16:31:37 +0300

    Autocomplete support added

--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -1,42 +1,35 @@
-const { Events } = require('discord.js');
+const { Events } = require("discord.js");
 
 module.exports = {
   name: Events.InteractionCreate,
-  execute(interaction) {
-
+  async execute(interaction) {
     if (interaction.isChatInputCommand()) {
       const command = interaction.client.commands.get(interaction.commandName);
       if (!command) return;
 
       try {
         command.execute(interaction);
-
       } catch (error) {
         console.error(error);
         return interaction.reply({
           content: `Komut çalıştırılırken bir hata çıktı! \n\n\`\`\`${error}\`\`\``,
           ephemeral: true,
-
         });
       }
 
-      console.log(`${interaction.user.tag} adlı kullanıcı /${interaction.commandName} komutunu kullandı. Kanal: #${interaction.channel.name} Sunucu: ${interaction.guild.name}` );
-
-    } else if(interaction.isButton()) {
-
+      console.log(
+        `${interaction.user.tag} adlı kullanıcı /${interaction.commandName} komutunu kullandı. Kanal: #${interaction.channel.name} Sunucu: ${interaction.guild.name}`
+      );
+    } else if (interaction.isButton()) {
       // Buttona tıklanınca olucak işlem kodlarını buraya koyun.
-
       /*
        Örnek:
         if(interaction.customId === "test") {
           return interaction.reply({ content: `Hey <@${interaction.user.id}>, Buttona bastın!` });
         }
       */
-
-    } else if(interaction.isSelectMenu()) {
-
+    } else if (interaction.isSelectMenu()) {
       // SelectMenu den birşey seçilince olucak işlem kodlarını buraya koyun.
-
       /*
        Örnek:
         if(interaction.customId === "test") {
@@ -44,28 +37,31 @@ module.exports = {
           return interaction.reply({ content: `Hey <@${interaction.user.id}>, Seçtiğin öğe: \`${secilen}\`!` });
         }
       */
-
-    } else if(interaction.isModalSubmit()) {
-
+    } else if (interaction.isModalSubmit()) {
       // ModalSubmit kodlarını buraya koyun.
-
-            /*
+      /*
        Örnek:
         if(interaction.customId === "test") {
           const soru1 = interaction.fields.getTextInputValue('soru1');
           return interaction.reply({ content: `Hey <@${interaction.user.id}>, Başarıyla modal submitledin! \n 1. Soruya yazdığın şey: ${soru1}` });
         }
       */
-
-    } else if(interaction.isUserContextMenuCommand()) {
-
-     // ContextMenuCommand kodlarını buraya koyun.
-
-     /*
+    } else if (interaction.isUserContextMenuCommand()) {
+      // ContextMenuCommand kodlarını buraya koyun.
+      /*
       Örnek:
        Bilmiyorum Linkteki yere bakın: https://discordjs.guide/interactions/context-menus.html#context-menus 
      */
-
+    } else if (interaction.isAutocomplete()) {
+      const { commands } = client;
+      const { commandName } = interaction;
+      const command = commands.get(commandName);
+      if (!command) return;
+      try {
+        await command.autocomplete(interaction, client);
+      } catch (err) {
+        console.error(err);
+      }
     }
   },
 };
